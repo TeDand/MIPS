afc56240cd537b99ff44fc495b9c578f3ec43177
TeDand <tejas-d@hotmail.com>
2019-11-05 09:33:35 +0000

    Added a load more r-type instructions

--- src/tejas.cpp
+++ src/tejas.cpp
@@ -8,15 +8,25 @@ const uint32_t IMEM_OFFSET = 0x10000000;
 const uint32_t IMEM_LENGTH = 0x1000000;
 const int WORD_LENGTH = 32;
 
-void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t& pc);
-void do_rType(const uint32_t &instr, uint32_t reg[],uint32_t& pc);
-void do_jType(const uint32_t &instr, uint32_t reg[],uint32_t& pc);
+void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t &pc);
+void do_rType(const uint32_t &instr, uint32_t reg[], uint32_t &pc);
+void do_jType(const uint32_t &instr, uint32_t reg[], uint32_t &pc);
 void decode_rType(const uint32_t &instr, int &r1, int &r2, int &dest, int &shft);
-int addu(const int &r1, const int &r2);
-void subu(const std::string &instr, uint32_t reg[]);
-void mul(const std::string &instr, uint32_t reg[]);
+int sll(const int &r2, const int &shft);
+int srl(const int &r2, const int &shft);
+int sra(const int &r2, const int &shft);
+int sllv(const int &r1, const int &r2);
+int srlv(const int &r1, const int &r2);
+int srav(const int &r1, const int &r2);
 int jr(const uint32_t &r1);
-
+//jalr
+//syscall
+int mfhi(uint32_t reg[]);
+int mflo(uint32_t reg[]);
+void mult(const int &r1, const int &r2, uint32_t reg[]);
+void multu(const int &r1, const int &r2, uint32_t reg[]);
+int addu(const int &r1, const int &r2);
+int subu(const int &r1, const int &r2);
 
 int main(int argc, char *argv[]) {
 
@@ -26,10 +36,10 @@ int main(int argc, char *argv[]) {
   std::vector<uint32_t> data_mem;
   data_mem.resize(0x4000000);
 
-  uint32_t registers[32] = {0};
+  uint32_t registers[34] = {0}; // 32 general purpose registers followed by; HI, LO
   uint32_t pc = 0;
 
-  char memblock[100];
+  char memblock[100]; // need to change later
   int sizeI = 0;
 
   std::ifstream binStream;
@@ -84,95 +94,164 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t& pc) {
+void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t &pc) {
   uint32_t opcode = instr & 0b11111100000000000000000000000000;
 
-  if (opcode == 0b00000000000000000000000000000000) {
+  if (opcode == 0) {
     do_rType(instr, reg,pc);
   }
-  else if ((opcode == 0b00001100000000000000000000000000) || (opcode == 0b00001000000000000000000000000000)) {
+  else if ((opcode == 0b1100000000000000000000000000) || (opcode == 0b1000000000000000000000000000)) {
     do_jType(instr,reg,pc);
   }
   else{
     std::cerr<<"itype"<<std::endl;
   }
-
 }
 
-void do_jType(const uint32_t &instr, uint32_t reg[],uint32_t& pc) {
-  
+void do_jType(const uint32_t &instr, uint32_t reg[],uint32_t &pc) {
+
   std::cerr << "Do J Type" << std::endl;
 }
 
-void do_rType(const uint32_t &instr, uint32_t reg[], uint32_t& pc) {
+void do_rType(const uint32_t &instr, uint32_t reg[], uint32_t &pc) {
   int r1, r2, dest, shft;
-  uint32_t fn = instr & 0b00000000000000000000000000111111;
+  uint32_t fn = instr & 0b111111;
 
   decode_rType(instr, r1, r2, dest, shft);
 
-  if (fn == 0b00000000000000000000000000100000) {
+  if (fn == 0b000000) {
+    std::cerr << "sll" << std::endl;
+    reg[dest] = sll(reg[r2], shft);
+  }
+  else if (fn = 0b000010) {
+    std::cerr << "srl" << std::endl;
+    reg[dest] << srl(reg[r2], shft);
+  }
+  else if (fn == 0b000011) {
+    std::cerr << "sra" << std::endl;
+    reg[dest] = sra(reg[r2], shft);
+  }
+  else if (fn == 0b000100) {
+    std::cerr << "sllv" << std::endl;
+    reg[dest] = sllv(reg[r1], reg[r2]);
+  }
+  else if (fn == 0b000110) {
+    std::cerr << "srlv" << std::endl;
+    reg[dest] = srlv(reg[r1], reg[r2]);
+  }
+  else if (fn == 0b000111) {
+    std::cerr << "srav" << std::endl;
+    reg[dest] = srav(reg[r1], reg[r2]);
+  }
+  else if (fn == 0b1000) {
+    std::cerr << "jr" << std::endl;
+    pc = jr(reg[r1]);
+  }
+  else if (fn == 0b010000) {
+    std::cerr << "mfhi" << std::endl;
+    reg[dest] = mfhi(reg);
+  }
+  else if (fn == 0b010001) {
+    std::cerr << "mthi" << std::endl;
+    reg[33] = reg[r1];
+  }
+  else if (fn == 0b010010) {
+    std::cerr << "mflo" << std::endl;
+    reg[dest] = mflo(reg);
+  }
+  else if (fn == 0b010011) {
+    std::cerr << "mtlo" << std::endl;
+    reg[34] = reg[r1];
+  }
+  else if (fn == 0b100001) {
     std::cerr << "addu" << std::endl;
     reg[dest] = addu(reg[r1], reg[r2]);
   }
-  else if(fn == 0b1000){
-    pc = jr(reg[r1]);
+  else if (fn == 0b100011) {
+    std::cerr << "subu" << std::endl;
+    reg[dest] = subu(reg[r1], reg[r2]);
+  }
+  else if (fn == 0b011000) {
+    std::cerr << "mult" << std::endl;
+    mult(reg[r1], reg[r2], reg);
+  }
+  else if (fn == 0b011001) {
+    std::cerr << "multu" << std::endl;
+    multu(reg[r1], reg[r2], reg);
   }
 
 }
 
-
 void decode_rType(const uint32_t &instr, int &r1, int &r2, int &dest, int &shft) {
-  uint32_t mask = 0b00000000000000000000000000011111;
+  uint32_t mask = 0b11111;
 
   shft = (instr >> 6) & mask;
-
   dest = (instr >> 11) & mask;
-
   r2 = (instr >> 16) & mask;
-
   r1 = (instr >> 21) & mask;
 
 }
 
-int addu(const int &r1, const int &r2) {
+int sll(const int &r2, const int &shft) { // logical shift left
+  return (r2 << shft);
+}
+
+int srl(const int &r2, const int &shft) { // logical shift right
+  int num = static_cast<uint32_t>(r2);
+  return (num >> shft);
+}
+
+int sra(const int &r2, const int &shft) { // arithmetic shift right
+  return (r2 >> shft);
+}
+
+int sllv(const int &r1, const int &r2) { // logical shift left using r2
+  return (r1 << r2);
+}
+
+int srlv(const int &r1, const int &r2) { // logical shift right using r2
+  int num = static_cast<uint32_t>(r1);
+  return (num >> r2);
+}
+
+int srav(const int &r1, const int &r2) { // arithmetic shift right using r2
+  return (r1 >> r2);
+}
+
+int jr(const uint32_t &r1){ // jump regeister
+    int target  = r1 - IMEM_OFFSET;
+    //target is the memory location - the instruction memory offset
+    if((target > 0) || (target < IMEM_LENGTH)){
+      return (target/4);
+    }
+}
+
+int mfhi(uint32_t reg[]) { // Move from HI register
+  return reg[33];
+}
+
+int mflo(uint32_t reg[]) {
+  return reg[34];
+}
+
+void mult(const int &r1, const int &r2, uint32_t reg[]) {
+  int64_t result = r1 * r2;
+  reg[34] = static_cast<uint32_t>(result);
+  reg[33] = static_cast<uint32_t>(result >> 32);
+}
+
+void multu(const int &r1, const int &r2, uint32_t reg[]) { // unsigned multiplication
+  uint32_t ur1 = static_cast<uint32_t>(r1);
+  uint32_t ur2 = static_cast<uint32_t>(r2);
+  uint64_t result = ur1 * ur2;
+  reg[34] = static_cast<uint32_t>(result);
+  reg[33] = static_cast<uint32_t>(result >> 32);
+}
+
+int addu(const int &r1, const int &r2) { // unsigned addition
   return r1 + r2;
 }
 
-void subu(const std::string &instr, uint32_t reg[]) {
-  std::string dest = instr.substr(6, 5);
-  std::string r1 = instr.substr(11, 5);
-  std::string r2 = instr.substr(16, 5);
-  // reg[10] = 8;
-  // reg[11] = 0;
-  // reg[9] = 4294967295;
-  reg[std::stoi(dest, nullptr, 2)] = reg[std::stoi(r1, nullptr, 2)] - reg[std::stoi(r2, nullptr, 2)];
-  // std::cerr << std::stoi(dest, nullptr, 2) << " = " << std::stoi(r1, nullptr, 2) << " - " << std::stoi(r2, nullptr, 2) << std::endl;
-  // std::cerr << reg[10] << std::endl;
-}
-
-void mul(const std::string &instr, uint32_t reg[]) {
-  std::string dest = instr.substr(6, 5);
-  std::string r1 = instr.substr(11, 5);
-  std::string r2 = instr.substr(16, 5);
-  // reg[10] = 8;
-  // reg[11] = 0;
-  // reg[9] = 4294967295;
-  reg[std::stoi(dest, nullptr, 2)] = reg[std::stoi(r1, nullptr, 2)] * reg[std::stoi(r2, nullptr, 2)];
-  // std::cerr << std::stoi(dest, nullptr, 2) << " = " << std::stoi(r1, nullptr, 2) << " * " << std::stoi(r2, nullptr, 2) << std::endl;
-  // std::cerr << reg[10] << std::endl;
-}
-
-int jr(const uint32_t &r1){
-    //000000 rs 0x15 001000
-    int target  = r1 - IMEM_OFFSET;
-    //target is the memory location - the instruction memory offset
-    if(target < 0 || target > IMEM_LENGTH){
-      std::cerr << "This should be an error message? This test has failed!!"<<std::endl;
-    }
-    else{
-      return (target/4);
-    }
-    std::cerr << "This is the target reg: "<< std::bitset<32>(target)<<std::endl;
- 
-
+int subu(const int &r1, const int &r2) { // unsigned subtraction
+  return r1 - r2;
 }
