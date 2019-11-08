afbf29bbbdb7e52754e0def6b7876d7fbd0e08a5
fmccallum <freddiemc42@gmail.com>
2019-11-08 16:13:58 +0000

    i dont want to lose this and i dont know how to pull but this program doesn't work

--- src/freddie.cpp
+++ src/freddie.cpp
@@ -8,14 +8,15 @@ const uint32_t IMEM_OFFSET = 0x10000000;
 const uint32_t IMEM_LENGTH = 0x1000000;
 const int WORD_LENGTH = 32;
 
-void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t& pc);
-void do_rType(const uint32_t &instr, uint32_t reg[],uint32_t& pc);
+void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t &pc,bool& isDelay);
+void do_rType(const uint32_t &instr, uint32_t reg[], uint32_t & pc);
 void do_jType(const uint32_t &instr, uint32_t reg[],uint32_t& pc);
 void decode_rType(const uint32_t &instr, int &r1, int &r2, int &dest, int &shft);
 int addu(const int &r1, const int &r2);
 void subu(const std::string &instr, uint32_t reg[]);
 void mul(const std::string &instr, uint32_t reg[]);
 int jr(const uint32_t &r1);
+uint32_t j(uint32_t index);
 
 
 int main(int argc, char *argv[]) {
@@ -25,7 +26,7 @@ int main(int argc, char *argv[]) {
 
   std::vector<uint32_t> data_mem;
   data_mem.resize(0x4000000);
-
+  //char* memblock = new char[sizeI];
   uint32_t registers[32] = {0};
   uint32_t pc = 0;
 
@@ -33,24 +34,27 @@ int main(int argc, char *argv[]) {
   int sizeI = 0;
 
   std::ifstream binStream;
-
+ 
   try {
-    binStream.open("assebly.mips.bin", std::ios::binary | std::ios::in);
+    binStream.open("simplJump.mips.bin", std::ios::binary | std::ios::in);
+    
     binStream.seekg (0, std::ios::end);
     std::streampos size = binStream.tellg();
     sizeI = size;
     binStream.seekg (0, std::ios::beg);
+     
     while (!binStream.eof()) {
       binStream.read(memblock, size);
     }
     binStream.close();
     binStream.clear();
+    
   }
   catch (...) {
     exit(-21); //no input file - exited with error
     std::cerr << "Input file does not exist" << std::endl;
   }
-
+  std::cerr << "Got here"<<std::endl;
   for (int i = 0; i < sizeI; i++){
     uint32_t tmp = static_cast<uint32_t>(memblock[i]) << 8 * (3-(i%4));
     instr_mem[i / 4] += tmp;
@@ -63,35 +67,113 @@ int main(int argc, char *argv[]) {
   registers[20] = 0x10000008;
   registers[22] = 0x1000000C;
 
-  find_instr(0b00000010110000000000000000001000, registers, pc);
+  bool isDelay = false; //true if executing next 
+  uint32_t dealySlot;
 
 
-  // while (1) {
-  //   uint32_t instruction;
-  //   if (pc == 0) {
-  //     std::cerr << "Program completed successfully" << std::endl;
-  //     exit(registers[2]); //program executed properly and exited
-  //   }
-  //   else if (pc < IMEM_OFFSET + IMEM_LENGTH) {
-  //     instruction = instr_mem[pc/4];
-  //   }
-  //   else {
-  //     std::cerr << "Accessing out of range memory" << std::endl;
-  //     exit(-11); //memory out of address range - exited with error
-  //   }
-  // }
+  while (1) {
+    std::string tBlock;
+    std::cout << std::endl;
+    std::cout << "Program counter = "<<std::hex << pc*4 + IMEM_OFFSET<<std::endl;
+    std::cout << "Vector index = "<<std::hex << pc<<std::endl;
+    std::cout << "And instruction is: " << std::bitset<32>(instr_mem[pc])<<std::endl;
+    std::cout << std::endl;
+    std::cin >> tBlock;
+
+    if(isDelay && (pc +1 < sizeI/4)){
+      isDelay = false;
+      find_instr(instr_mem[pc], registers,pc,isDelay);
+      if(isDelay){
+         dealySlot = instr_mem[pc];
+       }
+
+      pc+=1;
+
+    }
+    else if(dealySlot !=0){
+        find_instr(dealySlot, registers,pc, isDelay);
+        dealySlot = 0;
+        pc +=1;
+    }
+     else if (pc +1 < sizeI/4) {
+       find_instr(instr_mem[pc], registers,pc, isDelay);
+       if(isDelay){
+         dealySlot = instr_mem[pc];
+       }
+       pc += 1;
+     }
+     else if(pc +1 == sizeI/4){
+      std::cerr << "Program completed successfully" << std::endl;
+      exit(registers[2]); //program executed properly and exited
+     }
+     else {
+       std::cerr << "Accessing out of range memory" << std::endl;
+       exit(-11); //memory out of address range - exited with error
+     }
+   }
+
+   /*
+    while (1) {
+    
+    if(isDelay && (pc +1 < sizeI/4)){
+      isDelay = false;
+      find_instr(instr_mem[pc], registers,pc,isDelay);
+      if(isDelay){
+         dealySlot = instr_mem[pc];
+       }
+
+      pc+=1;
+
+    }
+    else if(dealySlot !=0){
+        find_instr(dealySlot, registers,pc, isDelay);
+        dealySlot = 0;
+        pc +=1; jo
+    }
+     else if (pc +1 < sizeI/4) {
+       find_instr(instr_mem[pc], registers,pc, isDelay);
+       if(isDelay){
+         dealySlot = instr_mem[pc];
+       }
+       pc += 1;
+     }
+     else if(pc +1 == sizeI/4){
+      std::cerr << "Program completed successfully" << std::endl;
+      exit(registers[2]); //program executed properly and exited
+     }
+     else {
+       std::cerr << "Accessing out of range memory" << std::endl;
+       exit(-11); //memory out of address range - exited with error
+     }
+   }
+   */
 
   return 0;
 }
 
-void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t& pc) {
-  uint32_t opcode = instr & 0b11111100000000000000000000000000;
 
-  if (opcode == 0b00000000000000000000000000000000) {
+// so if branch -> jump goes into delaySlot -> execute next -> execute delaySlot
+void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t &pc,bool& isDelay) {
+
+  uint32_t opcode = instr & 0b11111100000000000000000000000000;
+  if(instr == 0){
+
+  }
+  else if (opcode == 0b00000000000000000000000000000000) {
     do_rType(instr, reg,pc);
   }
   else if ((opcode == 0b00001100000000000000000000000000) || (opcode == 0b00001000000000000000000000000000)) {
-    do_jType(instr,reg,pc);
+    std::cerr<< "ITS A J"<< std::endl;
+    if(isDelay){
+      do_jType(instr,reg,pc);
+      isDelay = false;
+    }
+    else{
+      isDelay = true;
+    }
+      
+      
+    
   }
   else{
     std::cerr<<"itype"<<std::endl;
@@ -99,12 +181,20 @@ void find_instr(const uint32_t &instr, uint32_t reg[], uint32_t& pc) {
 
 }
 
-void do_jType(const uint32_t &instr, uint32_t reg[],uint32_t& pc) {
+void do_jType(const uint32_t &instr, uint32_t reg[],uint32_t & pc) {
   
   std::cerr << "Do J Type" << std::endl;
+  uint32_t jType = instr >> 26;
+  if(jType == 0b10){
+    pc = j(instr && 0X3FFFFFF) - (IMEM_OFFSET/4);
+  }
+  else{
+    std::cerr<<"No implemented instruction!!"<<std::endl;
+  }
+  
 }
 
-void do_rType(const uint32_t &instr, uint32_t reg[], uint32_t& pc) {
+void do_rType(const uint32_t &instr, uint32_t reg[], uint32_t & pc) {
   int r1, r2, dest, shft;
   uint32_t fn = instr & 0b00000000000000000000000000111111;
 
@@ -175,4 +265,11 @@ int jr(const uint32_t &r1){
     std::cerr << "This is the target reg: "<< std::bitset<32>(target)<<std::endl;
  
 
+}
+
+uint32_t j(uint32_t index){
+  //opcode index
+  //what about branch branch add
+  // aslo check this uses the delay slot
+  return index;
 }
